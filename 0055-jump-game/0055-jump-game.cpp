bab217950b2b1ce1e921e51df5b027d8e600ff8f
shreyaa <155134616+shreyaajahan@users.noreply.github.com>
2025-12-12 00:19:13 +0530

    Time: 0 ms (100%), Space: 52.3 MB (65.68%) - LeetHub

---
+++ 0055-jump-game/0055-jump-game.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    bool canJump(vector<int>& nums) {
+        int reach = 0;
+        for(int i = 0; i < nums.size(); i++) {
+            if(i > reach) return false;      
+            reach = max(reach, i + nums[i]); 
+            if(reach >= nums.size()-1) return true;
+        }
+        return true;
+    }
+};
