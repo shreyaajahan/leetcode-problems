d9bf25b1120ff1a4a703ebf7f647920b3e8224f4
shreyaa <155134616+shreyaajahan@users.noreply.github.com>
2026-01-17 01:13:23 +0530

    Time: 168 ms (18.35%), Space: 63.6 MB (5.34%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,34 +1,43 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
+        if (nums == null || nums.length < 3) return result;
+
         Arrays.sort(nums);
 
-        for (int i = 0; i < nums.length - 2; i++) {
+        // value -> last index
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            map.put(nums[i], i);
+        }
 
-            // Skip duplicate i
+        Set<String> used = new HashSet<>();
+
+        for (int i = 0; i < nums.length; i++) {
+
+            // skip duplicate i
             if (i > 0 && nums[i] == nums[i - 1]) continue;
 
-            int left = i + 1;
-            int right = nums.length - 1;
+            for (int j = i + 1; j < nums.length; j++) {
 
-            while (left < right) {
-                int sum = nums[i] + nums[left] + nums[right];
+                // skip duplicate j
+                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
 
-                if (sum == 0) {
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                int currSum = nums[i] + nums[j];
+                int target = -currSum;
 
-                    // Skip duplicates
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
+                if (map.containsKey(target)) {
+                    int k = map.get(target);
 
-                    left++;
-                    right--;
-                } 
-                else if (sum < 0) {
-                    left++;
-                } 
-                else {
-                    right--;
+                    // ensure distinct & ordered indices
+                    if (k > j) {
+                        String key = nums[i] + ":" + nums[j] + ":" + nums[k];
+
+                        if (!used.contains(key)) {
+                            result.add(Arrays.asList(nums[i], nums[j], nums[k]));
+                            used.add(key);
+                        }
+                    }
                 }
             }
         }
