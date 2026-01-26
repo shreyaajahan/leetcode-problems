c61e2725af6f11e4df0a25e2cf0d58b33fa4f5b9
shreyaa <155134616+shreyaajahan@users.noreply.github.com>
2026-01-26 23:07:14 +0530

    Time: 0 ms (100%), Space: 43.3 MB (55.9%) - LeetHub

--- 0289-game-of-life/0289-game-of-life.java
+++ 0289-game-of-life/0289-game-of-life.java
@@ -1,97 +1,48 @@
 class Solution {
     public void gameOfLife(int[][] board) {
-      int n=board.length;
-      int m=board[0].length;
-      int arr[][]=new int[n][m];  
-      for(int i=0;i<n;i++)
-      {
-        for(int j=0;j<m;j++)
-        {
-            int a=board[i][j];
-            int count=0;
-           if(i>0)
-           {
-             if(board[i-1][j]==1)
-             {
-                count++;
-             }
-           }
-           if(i<n-1)
-           {
-            if(board[i+1][j]==1)
-            {
-                count++;
+        int n = board.length;
+        int m = board[0].length;
+
+        // Extra matrix to store next state
+        int[][] arr = new int[n][m];
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                int a = board[i][j]; // current cell
+                int count = 0;       // live neighbors
+
+                // Check all 8 neighbors
+
+                if (i > 0 && board[i - 1][j] == 1) count++;          // up
+                if (i < n - 1 && board[i + 1][j] == 1) count++;     // down
+                if (j > 0 && board[i][j - 1] == 1) count++;         // left
+                if (j < m - 1 && board[i][j + 1] == 1) count++;     // right
+
+                if (i > 0 && j > 0 && board[i - 1][j - 1] == 1) count++;           // top-left
+                if (i < n - 1 && j < m - 1 && board[i + 1][j + 1] == 1) count++;   // bottom-right
+                if (i > 0 && j < m - 1 && board[i - 1][j + 1] == 1) count++;       // top-right
+                if (i < n - 1 && j > 0 && board[i + 1][j - 1] == 1) count++;       // bottom-left
+
+                // Apply Game of Life rules
+                if (a == 1 && count < 2) {
+                    arr[i][j] = 0;                 // underpopulation
+                } else if (a == 1 && (count == 2 || count == 3)) {
+                    arr[i][j] = 1;                 // lives
+                } else if (a == 1 && count > 3) {
+                    arr[i][j] = 0;                 // overpopulation
+                } else if (a == 0 && count == 3) {
+                    arr[i][j] = 1;                 // reproduction
+                } else {
+                    arr[i][j] = 0;                 // remains dead
+                }
             }
-           }
-           if(j>0)
-           {
-            if(board[i][j-1]==1)
-            {
-                count++;
-            }
-           } 
-           if(j<m-1)
-           {
-            if(board[i][j+1]==1)
-            {
-                count++;
-            }
-           }
-           if(i>0 && j>0)
-           {
-            if(board[i-1][j-1]==1)
-            {
-                count++;
-            }
-           }
-           if(i<n-1 && j<m-1)
-           {
-             if(board[i+1][j+1]==1)
-             {
-                count++;
-             }
-           }
-           if(i>0 && j<m-1)
-           {
-            if(board[i-1][j+1]==1)
-            {
-                count++;
-            }
-           }
-           if(i<n-1 && j>0)
-           {
-            if(board[i+1][j-1]==1)
-            {
-                count++;
-            }
-           }
-           if(a==1 && count<2)
-           {
-              arr[i][j]=0;
-           }
-           else if(a==1 && (count==2 || count==3))
-           {
-            arr[i][j]=1;
-           }
-           else if(a==1 && count>3)
-           {
-             arr[i][j]=0;
-           }
-           else if(a==0 && count==3)
-           {
-            arr[i][j]=1;
-           }
-           else{
-            arr[i][j]=0;
-           }
         }
-      }
-      for(int i=0;i<n;i++)
-      {
-        for(int j=0;j<m;j++)
-        {
-            board[i][j]=arr[i][j];
+
+        // Copy next state back to board
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                board[i][j] = arr[i][j];
+            }
         }
-      }
     }
 }
